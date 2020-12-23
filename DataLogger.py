ba74bbc0428c13e6c4623e04422c0456b2ca6745
boilingbrains <monsterpro01@gmail.com>
2020-12-23 01:24:29 +0100

    On continue les tests ajout imaghe to pixels

--- DataLogger.py
+++ DataLogger.py
@@ -1,22 +1,13 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 from sense_hat import SenseHat
 import time
-sense = SenseHat()
-sense.show_message("Hi")
-X = [255, 0, 0]  # Red
-O = [255, 255, 255]  # White
+import cv2
+import matplotlib.pyplot as plt
 
-question_mark = [
-O, O, O, X, X, O, O, O,
-O, O, X, O, O, X, O, O,
-O, O, O, O, O, X, O, O,
-O, O, O, O, X, O, O, O,
-O, O, O, X, O, O, O, O,
-O, O, O, X, O, O, O, O,
-O, O, O, O, O, O, O, O,
-O, O, O, X, O, O, O, O
-]
-
-sense.set_pixels(question_mark)
-time.sleep(5)
-sense.clear()
+path = 'babyyoda.gif'
+image= cv2.imread(read)
+image = cv2.resize(image,(8,8))
+plt.imshow(image)
+#sense = SenseHat()
+#sense.show_message("Hi")
+#sense.clear()
