3c3296f3427ef4a546e14a6e8471e183d4361b8b
Ibrahim Khaled <ikhaled@ulb.ac.be>
2020-12-26 23:33:39 +0100

    create Display file and new Datalogger file

--- DataLogger.py
+++ DataLogger.py
@@ -1,13 +1,12 @@
 #! /usr/bin/python3
-from sense_hat import SenseHat
-import time
-import cv2
-import matplotlib.pyplot as plt
 
-path = 'babyyoda.gif'
-image= cv2.imread(read)
-image = cv2.resize(image,(8,8))
-plt.imshow(image)
-#sense = SenseHat()
-#sense.show_message("Hi")
-#sense.clear()
+#Modules
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+#Collect Data
+pressure = sense.get_pressure()
+temp = sense.get_temperature()
+hum = sense.get_humidity()
---
+++ Display.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+from sense_hat import SenseHat
+import time
+import cv2
+import matplotlib.pyplot as plt
+
+path = 'babyyoda.gif'
+image= cv2.imread(path)
+image = cv2.resize(image,(8,8))
+plt.imshow(image)
+#sense = SenseHat()
+#sense.show_message("Hi")
+#sense.clear()
