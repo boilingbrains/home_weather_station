9ba951b6b9d89e48065b29e2fa0a69db06d30e70
Ibrahim Khaled <ikhaled@ulb.ac.be>
2021-01-01 19:49:05 +0100

    add display part on DataLogger code

--- DataLogger.py
+++ DataLogger.py
@@ -3,6 +3,8 @@
 #Modules
 from sense_hat import SenseHat
 import numpy as np
+import cv2
+import os
 sense = SenseHat()
 sense.clear()
 
@@ -18,6 +20,23 @@ hum = sense.get_humidity()
 
 
 #Display
+def match(element):
+    return bool(element[len(element)-3:len(element)] == 'png' )
+path = os.getcwd()+'/images/'
+images = [i for i in os.listdir(path)]
+images = list(filter(match,images))
+#print(images)
+for i in images:
+    print(i)
+    image = cv2.imread(path + i)
+    image = cv2.resize(image,(8,8))
+    sense.load_image(image)
+    sleep(0.2)
+    #cv2.imshow(i,image)
+    #cv2.waitKey(0) 
+#cv2.destroyAllWindows()        
+sense.clear()
+print("voilà")
 
 ####
 # Intro Animation
